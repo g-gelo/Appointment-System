28d164350775b7c0e70c658c68e308d92e066d29
Justine Licuanan <caissoone@gmail.com>
2022-12-06 18:13:35 +0800

    first commit

---
+++ resources/views/admin/layout/logo.blade.php
@@ -0,0 +1,7 @@
+<a href="{{ url('admin') }}" class="navbar-brand">
+    {{-- You may use plain text as a logo instead of image --}}
+    <span>Smith Dental Clinic</span>
+
+    {{--Text Logo--}}
+
+</a>
